e7bfdedf37286a54c18dd886c538df035c8c1f10
GokulVSD <gokul.vasda@gmail.com>
2020-03-25 22:11:14 +0530

    Added utilities that will be required later

--- point_cloud.py
+++ point_cloud.py
@@ -8,6 +8,33 @@ from open3d import read_point_cloud, draw_geometries
 # Instructions on how to control the view:
 # http://www.open3d.org/docs/release/tutorial/Basic/visualization.html
 
+"""
+
+-- Mouse view control --
+  Left button + drag         : Rotate.
+  Ctrl + left button + drag  : Translate.
+  Wheel button + drag        : Translate.
+  Shift + left button + drag : Roll.
+  Wheel                      : Zoom in/out.
+
+-- Keyboard view control --
+  [/]          : Increase/decrease field of view.
+  R            : Reset view point.
+  Ctrl/Cmd + C : Copy current view status into the clipboard.
+  Ctrl/Cmd + V : Paste view status from clipboard.
+
+-- General control --
+  Q, Esc       : Exit window.
+  H            : Print help message.
+  P, PrtScn    : Take a screen capture.
+  D            : Take a depth capture.
+  O            : Take a capture of current rendering settings.
+
+  Ctrl + c     : Save view point as JSON to clipboard
+  Ctrl + v     : Load view point from clipboard
+
+"""
+
 focalLength = 525.0
 centerX = 319.5
 centerY = 239.5
--- predictor.py
+++
@@ -1,6 +0,0 @@
-# from 'some_place' import predictAndSave
-
-def generate_depth_map():
-    # predictAndSave()
-    pass
-
