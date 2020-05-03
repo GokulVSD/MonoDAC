aad10ca9073c10603e42957cc56af3c813fc2bb7
GokulVSD <gokul.vasda@gmail.com>
2020-05-04 02:14:49 +0530

    More progress on evaluation

--- evaluate.py
+++ evaluate.py
@@ -7,19 +7,30 @@ from PIL import Image
 TEST_DIR = "./data/nyu2_test/"
 TEMP_DIR = "./data/temp/"
 
+# Calculates Absolute Relative Difference, Squared Relative Difference, RMS Linear and RMS Log for 1 pair of images
+def calculate_metrics(depth, pred):
+    t = 0
+    ard_acc = 0
+    srd_acc = 0
+    for x in range(depth.size[0]):
+        for y in range(depth.size[1]):
 
-def ard(rgb, depth, total):
-    pass
+            t += 1
+            z, z_pred = None, None
 
-def srd(rgb, depth, total):
-    pass
+            if type(depth.getpixel((x,y))) == tuple:
+                z = depth.getpixel((x,y))[0]
+            else:
+                z = depth.getpixel((x,y))
 
-def rms_linear(rgb, depth, total):
-    pass
+            if type(pred.getpixel((x,y))) == tuple:
+                z_pred = pred.getpixel((x,y))[0]
+            else:
+                z_pred = pred.getpixel((x,y))
 
-def rms_log(rgb, depth, total):
-    pass
+            acc += abs(z_pred - z)/z
 
+    return acc/t
 
 
 rgb_names = []
@@ -43,21 +54,16 @@ if not os.path.exists(TEMP_DIR):
         prediction_names.append(TEMP_DIR + str(i) + ".png")
         generate_depth_map_for_evaluation(rgb_names[i], prediction_names[i])
 
-    print("\n\n Done. \n\n")
+    print("\n\nDone. \n\n")
 else:
     print("Using predicted depth maps from previous run. \n\n")
     for i in range(total):
         prediction_names.append(TEMP_DIR + str(i) + ".png")
 
-rgb = []
+
 depth = []
 pred = []
 
-for i in range(total):
-    print("Loading RGB images into memory: ",i+1," of ",total, end="\r")
-    rgb.append(Image.open(rgb_names[i]))
-
-print("\n")
 
 for i in range(total):
     print("Loading depth maps into memory: ",i+1," of ",total, end="\r")
@@ -69,7 +75,17 @@ for i in range(total):
     print("Loading predicted depth maps into memory: ",i+1," of ",total, end="\r")
     pred.append(Image.open(prediction_names[i]))
 
-print("\n\n Done. \n\n")
+print("\n\nDone. \n\n")
 
+ard_acc = 0
+srd_acc = 0
+rms_linear_acc = 0
+rms_log_acc = 0
+
+for i in range(total):
+    print("Calculating metrics for: ",i+1," of ",total, end="\r")
+    res = ard(depth[i], pred[i])
+
+print("\nAverage Absolute Relative Difference (ARD): ", cumulative/total, "\n\n")
 
 
